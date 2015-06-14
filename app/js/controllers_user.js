2458e71b216ad2bbf8b19979765d6803246a5528
srldl <siri.uldal@gmail.com>
2015-06-14 11:14:52 +0200

    fixed date format in forms

--- app/js/controllers_user.js
+++ app/js/controllers_user.js
@@ -82,6 +82,10 @@ sightingControllers.controller('EditObservationCtrl',  function($scope, $routePa
   Sighting, SightingDBUpdate, npolarApiSecurity, npolarApiUser) {
     var entry = SightingDBUpdate.get({id: $routeParams.id }, function(){
     $scope.entry = entry;
+    /*Set abbreviated dates*/
+    $scope.entry.event_date = entry.event_date.substring(0,10);
+    $scope.entry.expedition.start_date = entry.expedition.end_date.substring(0,10);
+    $scope.entry.expedition.end_date = entry.expedition.end_date.substring(0,10);
      console.log('edit ' + JSON.stringify(entry));
     $scope.items = species_gallery;
   });
