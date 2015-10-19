46b095a12d60b4f87ffeb275f5ea49ef174964b3
srldl <siri.uldal@gmail.com>
2015-10-19 10:21:19 +0200

    map returning search

--- src/js/AdminObservationsCtrl.js
+++ src/js/AdminObservationsCtrl.js
@@ -37,7 +37,7 @@ $scope.isAdmin = function() {
       }
   });
 
-     //Draw a rectangle on the map to get coordinates from
+  //Draw a rectangle on the map to get coordinates from
   leafletData.getMap().then(function(map) {
 
         var drawnItems = new L.featureGroup().addTo(map);
@@ -57,20 +57,16 @@ $scope.isAdmin = function() {
                 $scope.lng1= res[0][0][1];
                 $scope.lat2= res[0][2][0];
                 $scope.lng2= res[0][2][1];
-                console.log($scope);
-                console.log("----");
         });
 
-
-        //
-         map.on('draw:edited', function (e) {
+        map.on('draw:edited', function (e) {
 
            var layers = e.layers;
            layers.eachLayer(function (layer) {
-             /*update lng/lat from search */
+             //Update lng/lat from search
              var res = (layer.toGeoJSON()).geometry.coordinates;
 
-                /*fetch zero and second coordinate pair to get a rectangle */
+                //Fetch zero and second coordinate pair to get a rectangle
                 $scope.lat1= res[0][0][0];
                 $scope.lng1= res[0][0][1];
                 $scope.lat2= res[0][2][0];
@@ -87,11 +83,122 @@ $scope.isAdmin = function() {
          //Remove markers and squares
          $scope.markers = [];
         });
-        //
 
-});
 
-};
+}); //leafletdata.getMap
+
+  // Execute this function when advanced search button is pressed
+ $scope.submit = function() {
+
+    // First find out which paramaters are not empty
+    var sok = ''; var lat = ''; var lng = ''; var edate = '';
+
+
+    // If event_date exists
+    if (typeof $scope.event_date1 != "undefined" && $scope.event_date1 != "") {
+           //Remember to transform into the correct format
+           edate = '&filter-event_date=' + convertDate($scope.event_date1) + '..';
+
+           if (typeof $scope.event_date2 != "undefined" && $scope.event_date2 != "") {
+               //Transform edate to correct format
+               edate = edate + convertDate($scope.event_date2);
+
+           }
+    //Else if lat2 exists
+    } else if (typeof $scope.event_date2 != "undefined" && $scope.event_date2 != "") {
+               //Transform edate to correct format
+               edate = '&filter-event_date=..' + convertDate($scope.event_date2);
+    }
+
+
+    // If lat1 exists
+    if (typeof $scope.lat1 != "undefined" && $scope.lat1 != "") {
+           lat = '&filter-latitude=' + $scope.lat1 + '..';
+
+
+           if (typeof $scope.lat2 != "undefined" && $scope.lat2 != "") {
+               lat = lat + $scope.lat2;
+           }
+    // Else if lat2 exists
+    } else if (typeof $scope.lat2 != "undefined" && $scope.lat2 != "") {
+               lat = '&filter-latitude=..' + $scope.lat2;
+    }
+
+
+    // If lng1 exists
+    if (typeof $scope.lng1 != "undefined" && $scope.lng1 != "") {
+           lng = '&filter-longitude=' + $scope.lng1 + '..';
+
+           //If both exists
+           if (typeof $scope.lng2 != "undefined" && $scope.lng2 != "") {
+               lng = lng + $scope.lng2;
+
+           }
+    //Else if lng2 exists
+    } else if (typeof $scope.lng2 != "undefined" && $scope.lng2 != "") {
+               lng = '&filter-longitude=..' + $scope.lng2;
+
+    }
+
+    //Include species search if it exists
+    if ((typeof $scope.species != "undefined") && ($scope.species != null) && ($scope.species != '' )) {
+           sok = sok + '&filter-species=' + ($scope.species.family).toLowerCase();
+           sok = sok.replace(/ /g,"+");
+    };
+
+    //Sum up the query
+    if ($scope.search) {
+       sok = $scope.search;
+    }else {
+       sok = sok+lat+lng+edate;
+    }
+
+
+    console.log(sok);
+
+
+    $http.jsonp('https://api.npolar.no/sighting/?q='+ sok +'&format=json&callback=JSON_CALLBACK&locales=utf-8').success(function(data) {
+    $scope.full = data;
+    console.log(data);
+
+    var redIcon = {
+    iconUrl: 'img/icons/reddot.png',
+    iconSize:     [8, 8] // size of the icon
+    }
+
+    // Fetch the lat/lon entries. Have to switch lat/lon for display
+    for (var i=0; i< $scope.full.feed.entries.length; i++) {
+       markers.push({
+                lat: parseFloat($scope.full.feed.entries[i].longitude),
+                lng: parseFloat($scope.full.feed.entries[i].latitude),
+                focus: true,
+                draggable: false,
+                message: $scope.full.feed.entries[i].locality,
+                icon: redIcon
+       });
+    };
+
+    //Display markers on map
+    $scope.markers = markers;
+
+    //Reset for next search
+    markers = [];
+
+    //Display data for all entries
+    $scope.entries = $scope.full.feed.entries;
+
+    //Transfer info to CSV file via service
+    CSVService.entryObject = $scope.entries;
+
+    //Get hostname
+    $scope.hostname = location.host;
+   // console.log($scope.hostname);
+
+  });
+
+}; //submit
+
+};  //AdminObservationsCtrl
 
 /*Convert to the search date format */
 function convertDate(idate) {
