c125d84f00f4952fb720e8093189aa79b492487f
srldl <siri.uldal@gmail.com>
2015-11-12 15:28:23 +0100

    added links new passwd+login

--- src/js/UploadImagesCtrl.js
+++ src/js/UploadImagesCtrl.js
@@ -5,9 +5,6 @@
 // @ngInject
 var UploadImagesCtrl = function($scope, $http, NpolarApiSecurity, Sighting) {
      $scope.security = NpolarApiSecurity;
-    // $scope.pictures = [{filename:'bird2.jpg', content_type:'jpg', content_size:'6534', photographer:'hans olsen',
-    // comments:'best img ever!', other_info:'not great!'}];
-
 
      // Dataset -> npolarApiResource -> ngResource
     //$scope.resource = Sighting;
