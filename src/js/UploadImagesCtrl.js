0dcc573ebf69e8242623f5ae92532211c27546c2
srldl <siri.uldal@gmail.com>
2015-11-12 07:59:28 +0100

    image upload directive in progress

--- src/js/UploadImagesCtrl.js
+++ src/js/UploadImagesCtrl.js
@@ -5,11 +5,23 @@
 // @ngInject
 var UploadImagesCtrl = function($scope, $http, NpolarApiSecurity, Sighting) {
      $scope.security = NpolarApiSecurity;
+     console.log("controller");
+   //  $scope.pictures = [{filename:'bird2.jpg', content_type:'jpg', content_size:'6534', photographer:'hans olsen',
+   //  comments:'best img ever!', other_info:'not great!'}];
+   //  console.log($scope.pictures);
+     if ($scope.pictures && $scope.pictures !== "null" &&  $scope.pictures !== "undefined") {
+         $scope.show = "true";
+     } else {
+         $scope.show = "false";
+     };
 
      // Dataset -> npolarApiResource -> ngResource
     //$scope.resource = Sighting;
-    $scope.color = "red";
-    $scope.format = 'M/d/yy h:mm:ss a';
+    $scope.submit = function() {
+       console.log($scope);
+       console.log("submit");
+    };
+
 
 };
 
--- src/js/picture.js
+++ src/js/picture.js
@@ -4,20 +4,47 @@
  * @ngInject
  */
 var picture = function () {
-   return {
-      restrict: 'AE',
-      require: '?ngModel',
-     // templateUrl: './src/js/picture-upload.html',
-      link: function(scope, element, attrs, ngModel) {
-         console.log("ngModel", ngModel);
-      	 console.log(attrs.tester);
-      	 console.log("scope", scope);
-      	 console.log("scope color", scope.color);
-      	 console.log("element", element);
+    return {
+      restrict: 'A',
+      //require: '?ngModel',
+      templateUrl: 'partials/user/picture-upload.html',
+      scope: {
+          pictures: "=",
+          show:"="
+      },
+      link: function(scope, elem, attrs, ctrl) {
+        console.log("SCOPE", scope);
+        console.log("ELEM", elem);
+        console.log("THIS", angular.element(this));
+      //  scope.form = {submitted: false, data: {}};
 
+
+      scope.filesChanged = function(e){
+        scope.files=e.files;
+        console.log(scope.show);
+
+        //If scope.pictures does not exists
+        if (!scope.pictures || scope.pictures === "null" ||  scope.pictures === "undefined") {
+           console.log("create pictures obj");
+           scope.pictures = [];
+        };
+
+         //Transfer uploaded pictures to
+        for (var i = 0; i < scope.files.length; i++) {
+           var obj = new Object();
+           obj.filename = scope.files[i].name;
+           obj.content_type = scope.files[i].type;
+           obj.content_size = scope.files[i].size;
+           (scope.pictures).push(obj);
+        }
+        scope.show = "true";
+        console.log(scope);
+       // scope.apply();
+      };
 
       }
-  };
+  }
 };
+
 
 module.exports = picture;
